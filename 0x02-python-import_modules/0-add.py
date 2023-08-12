e404ff0b0f43db87cdcf42206e3a05028b593152
wina127 <wongelawitalemayhu@gmail.com>
2023-08-11 19:22:49 -0700

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/user/bin/python3
 
-if _name_ == "_main_":
-    """print the sum of 1 and 2."""
-    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    def add(a, b):
+        return a + b
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
