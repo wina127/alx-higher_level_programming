46c4cc720e1cdb347c29396560f0ee6828e2bd26
wina127 <wongelawitalemayhu@gmail.com>
2023-08-13 19:30:53 +0000

    add py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/user/bin/python3
-
-    a = 1
-    b = 2
-    def add(a, b):
-        return a + b
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+"""Import from add.py"""
+if __name__ == "__main__":
+from add_0 import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
