81e7fbc2128a6dbfb6b5cb5d779b9cc4625d75a9
wina127 <wongelawitalemayhu@gmail.com>
2023-08-13 12:42:13 -0700

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,7 @@
 #!/user/bin/python3
 """Import from add.py"""
-<<<<<<< HEAD
-if _name_ == "_main_":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-=======
 if __name__ == "__main__":
 from add_0 import add
 a = 1
 b = 2
 print("{} + {} = {}".format(a, b, add(a, b)))
->>>>>>> 46c4cc720e1cdb347c29396560f0ee6828e2bd26
