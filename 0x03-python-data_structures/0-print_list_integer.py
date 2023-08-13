35e5b4808f776f185923501b3aabe8bf937b36f1
wina127 <wongelawitalemayhu@gmail.com>
2023-08-13 14:32:47 -0700

    multiple returns

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """prints all integers of a list
+    """
+    prints all integers of a list
+
     args:
-    my_list: the list argument whose items will be printed """
+    my_list: the list argument whose items will be printed
+    """
 for i in my_list:
-    print("{:d}".format(integer))
+    print("{:d}".format(i))
+    #print numbers 1-5
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
