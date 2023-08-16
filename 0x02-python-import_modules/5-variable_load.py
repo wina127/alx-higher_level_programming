68094269cc00aa858ff9ca1cdfe9512757098756
wina127 <wongelawitalemayhu@gmail.com>
2023-08-16 15:59:41 -0700

    my calculator

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """print the value of variable a from variable_load_5."""
-    for variable_load_5 import a
+    for variable_load_5.py import a
     print(a)
