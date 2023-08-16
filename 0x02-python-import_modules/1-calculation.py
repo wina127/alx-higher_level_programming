c7ca4f80c63f9eb6ed20c59153c0fe9dee6f2332
wina127 <wongelawitalemayhu@gmail.com>
2023-08-16 10:35:23 -0700

    calculat'

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,6 @@ if _name_ == "_main_":
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
-    print
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+    print('{:D} / {:d} = {:d}'.format(a, b, (div(a, b))))
