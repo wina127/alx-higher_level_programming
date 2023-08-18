14ac7c1872d20fb3cbaad20eda289e570b8d2354
wina127 <wongelawitalemayhu@gmail.com>
2023-08-18 07:27:03 -0700

    last

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,8 @@ last_digit = abs(number) % 10
 if number < 0:
     last_digit *= -1
 
-#approprite message 
+#approprite message
+
     if last_digit > 5:
         message = "is greater than 5"
     elif last_digit == 0:
