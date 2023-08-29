77a4b05e8a3af4673b19ecce8ff25bf2515710b8
wina127 <wongelawitalemayhu@gmail.com>
2023-08-29 14:12:04 -0700

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+
+    """print x elements of a list"""
+
     total = 0
+
     for i in range(x):
+
         try:
+
             print(f"{my_list[i]}", end="")
+
             total += 1
+
         except IndexError:
+
             break
-        print()
+
+        print("")
+
         return(total)
