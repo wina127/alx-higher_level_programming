7463dc4d3829560a64da3229723f78ca0dcabccc
wina127 <wongelawitalemayhu@gmail.com>
2023-08-29 16:14:38 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-square.py by UDO INNOCENT
+"""Defines a square """
+
+class square:
+    """Represents a square"""
+
+    def __int__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
