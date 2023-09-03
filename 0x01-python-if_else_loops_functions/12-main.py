3de2d50d663bb4204d80791177caa8dc43e4bf24
wina127 <122681290+wina127@users.noreply.github.com>
2023-09-03 12:54:23 -0600

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/12-main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-FizzBuzz = __import__('12-fizzbuzz').FizzBuzz
-
-FizzBuzz()
-print("")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def islower(c):
-    """function checks for lowercase characters."""
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
